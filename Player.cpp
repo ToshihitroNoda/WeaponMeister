924847c47cf45dd4a194aa785fafe552c0ab9278
Noda <toushiiiiii@gmail.com>
2021-06-05 01:52:17 +0900

    プレイヤーの斜め移動を滑らかにするのの途中

--- Player.cpp
+++ Player.cpp
@@ -69,13 +69,25 @@ void Player::HandleInput()
 		}
 
 		if (Input::GetButton(PAD_INPUT_8) && Input::GetButton(PAD_INPUT_4)) // 左前
-			modelAngle_ = -((nowCamAngle_ - 45) * MyMath::Deg2Rad);
+		{
+			if (modelAngle_ != -((nowCamAngle_ - 45) * MyMath::Deg2Rad))
+				modelAngle_ -= 0.5f;
+		}
 		else if (Input::GetButton(PAD_INPUT_8) && Input::GetButton(PAD_INPUT_6)) // 右前
-			modelAngle_ = -((nowCamAngle_ - 135) * MyMath::Deg2Rad);
+		{
+			if (modelAngle_ != -((nowCamAngle_ - 135) * MyMath::Deg2Rad))
+				modelAngle_ += 0.5f;
+		}
 		else if (Input::GetButton(PAD_INPUT_5) && Input::GetButton(PAD_INPUT_4)) // 左後ろ
-			modelAngle_ = -((nowCamAngle_ + 45) * MyMath::Deg2Rad);
+		{
+			if (modelAngle_ != -((nowCamAngle_ + 45) * MyMath::Deg2Rad))
+				modelAngle_ += 0.5f;
+		}
 		else if (Input::GetButton(PAD_INPUT_5) && Input::GetButton(PAD_INPUT_6)) // 右後ろ
-			modelAngle_ = -((nowCamAngle_ + 135) * MyMath::Deg2Rad);
+		{
+			if (modelAngle_ != -((nowCamAngle_ + 135) * MyMath::Deg2Rad))
+				modelAngle_ -= 0.5f;
+		}
 
 		if (!Input::GetButton(PAD_INPUT_4) &&
 			!Input::GetButton(PAD_INPUT_6) &&
